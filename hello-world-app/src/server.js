1742f8b6f93fd1fa54f65b2a56976aa03d1405cb
Krzysztof Zwolenik <kzwolenik95@gmail.com>
2025-02-13 18:42:47 +0100

    Add secret endpoint to the service

--- hello-world-app/src/server.js
+++ hello-world-app/src/server.js
@@ -1,11 +1,24 @@
-const express = require('express');
+const express = require("express");
+const basicAuth = require("express-basic-auth");
+
 const app = express();
 const port = 80;
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+const auth = basicAuth({
+  users: {
+    [process.env.USERNAME]: process.env.PASSWORD,
+  },
+  challenge: true,
 });
 
-app.listen(port, '0.0.0.0', () => {
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.get("/secret", auth, (req, res) => {
+  res.send(process.env.SECRET_MESSAGE);
+});
+
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server running on ${port}`);
 });
